4853e829ab9ca0356c08fdfd47caddb416028a7b
Alina <varkkia99@gmail.com>
2020-04-13 20:19:10 +0100

    first menu kinda ready

---
+++ MathsCube/Assets/Scripts/MenuFloat.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuFloat : MonoBehaviour
+{
+    public Vector3 moveDirection;
+    public float moveDistance;
+    public float moveSpeed;
+
+    private Vector3 startPosition;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = gameObject.transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = startPosition + moveDirection * (moveDistance * Mathf.Sin(Time.time * moveSpeed));
+    }
+}
---
+++ MathsCube/Assets/Scripts/OnClickCube.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class OnClickCube : MonoBehaviour
+{
+    public GameObject cube;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseUp()
+    {
+        if (cube)
+        {
+            Debug.Log("Clickeeeeeeeeed");
+        }
+    }
+}

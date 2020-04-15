f883cdd2c7cd05aa6e13bad91568c5fc099e5761
Alina <varkkia99@gmail.com>
2020-04-15 11:32:15 +0100

    pause menu just created

---
+++ MathsCube/Assets/PauseMenu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+  public Image pause;
+    public GameObject ui;
+   
+    public void PauseCLicked()
+    {
+        Toggle();
+        
+    }
+
+    public void Toggle()
+    {
+        ui.SetActive(!ui.activeSelf);
+
+        if (ui.activeSelf)
+        {
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
+    }
+
+    public void Retry()
+    {
+        Toggle();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Menu()
+    {
+        Toggle();
+        SceneManager.LoadScene(0);
+    }
+}

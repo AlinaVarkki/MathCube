587e375a63b2f1ce45da7df208ca23e82df8cd54
Alina <varkkia99@gmail.com>
2020-04-15 14:55:41 +0100

    fading unlocked levels

--- MathsCube/Assets/Scripts/LevelSelector.cs
+++ MathsCube/Assets/Scripts/LevelSelector.cs
@@ -1,8 +1,18 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelSelector : MonoBehaviour
 {
     public SceneFader fader;
+    public Button[] levelButtons;
+
+    private void Start()
+    {
+        for (int i = 0; i < levelButtons.Length; i++)
+        {
+           levelButtons[i].interactable = false;
+        }
+    }
 
     public void Select(string levelName)
     {

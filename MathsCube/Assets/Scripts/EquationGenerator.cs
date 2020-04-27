600d5dfa550300c22158144dd52dca5535f6b938
Alina <varkkia99@gmail.com>
2020-04-27 12:47:18 +0100

    changed fornt to Roboto

--- MathsCube/Assets/Scripts/EquationGenerator.cs
+++ MathsCube/Assets/Scripts/EquationGenerator.cs
@@ -26,6 +26,9 @@ public class EquationGenerator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        equation.fontSize = 65;
+        
+
         currentScene = SceneManager.GetActiveScene().buildIndex;
 
 
